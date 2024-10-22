514209681a1d9d05b52a16f52b31c02d1c411862
Davis Keene <dbkeene.tsyc@gmail.com>
2024-10-22 08:27:36 -0400

    fix: popover open down (2)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,25 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
+
+  // iOS keyboard fix
+  React.useEffect(() => {
+    const handleResize = () => {
+      if (document.activeElement?.tagName === 'INPUT') {
+        window.setTimeout(() => {
+          document.activeElement?.scrollIntoView({ block: 'center' });
+        }, 0);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener when component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  
   return (
     <QueryClientProvider client={queryClient}>
       <AppRoutes />
