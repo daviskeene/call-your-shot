f5a43bff93d40b5c08c14657899f56df808d7399
Davis Keene <dbkeene.tsyc@gmail.com>
2024-11-11 00:34:37 -0500

    fix: support both sqlite and postgres

--- backend/app/database.py
+++ backend/app/database.py
@@ -25,9 +25,11 @@ Base = declarative_base()
 
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
+    if DATABASE_URL.startswith("sqlite"):            
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
+    
 
 
 def init_db():
